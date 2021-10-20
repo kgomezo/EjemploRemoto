2ba7d146ff932d3f8ae5203e1336a0535046433b
Karen <kgomezo@unal.edu.co>
2021-10-20 12:58:01 -0500

    Tarea plantilla

--- NumericalErrors-14-10/plantillahw.cpp
+++ NumericalErrors-14-10/plantillahw.cpp
@@ -1,9 +1,12 @@
 #include<iostream>
 #include <cmath>
+#include<stdlib.h>
+#include<math.h>
 //calcular el sin(x=pi/3), usando la expansion matematica
 //sin (x,y) = \sum_{n=1}{N} (-1){n-1} x{2n-1}/(2n-1)!
 
 double mysin(double x, int N);
+int fact(int a);
 
 int main(){
   std::cout.setf(std::ios::scientific);
@@ -18,11 +21,21 @@ int main(){
   }
   return 0;
 }
+int fact(int a){
+  int F=1;
+  for(int i=1; i<= a; i++){
+    F=F*i;
+  }
+  return F;
+}
 
 double mysin(double x, int N)
 {
-  //implementar la suma y hacer la figura
+  double seno=0;
+  for (int n=0; n<= N; ++n)
+    {
+      seno= seno + (std::pow(-1,n-1))*(std::pow(x,(2*n)-1))/(fact((2*n)-1));
+    }
 
-
-  return 0;
+  return seno;
 }

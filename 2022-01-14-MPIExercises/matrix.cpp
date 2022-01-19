72fad24b1a33a52029f708d779911d658459da8e
kgomezo <kgomezo@unal.edu.co>
2022-01-19 09:50:03 -0500

    j

--- 2022-01-14-MPIExercises/matrix.cpp
+++ 2022-01-14-MPIExercises/matrix.cpp
@@ -5,7 +5,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void printdata(std::vector<double>&data,int N, int Nlocal);
+void printdata(std::vector<double>data,int N, int Nlocal);
 
 
 int main(int argc, char **argv)
@@ -15,30 +15,34 @@ int main(int argc, char **argv)
   int Nlocal=N/np;
   int tag =0;
 
+    std::vector<double>data(N*Nlocal);
+    std::fill(data.begin(),data.end(),0.0);
+
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &np);
     MPI_Comm_rank(MPI_COMM_WORLD, &pid);
     MPI_Status status;
 
-    std::vector<double>data(N*Nlocal);
-    std::fill(data.begin(),data.end(),0.0);
+
    
 
     for(int ilocal=0; ilocal<Nlocal;++ilocal){
 
         for(int jlocal = Nlocal*pid; jlocal < Nlocal*(pid+1);++jlocal) {
-            data[ilocal*(N)+jlocal]=1.0;
+            data[ilocal*(N+1)+jlocal]=1.0;
             //break;
 
         }
 
     }
+
    
     
     double totaltime = 0;
 
     if(pid == 0){
-        printdata(data,N,Nlocal);
+      printdata(data,N,Nlocal);
+
         for(int ipid=1; ipid < np;++ipid){
 	  double start= MPI_Wtime();
             MPI_Recv(&data[0], N*Nlocal, MPI_DOUBLE, ipid,tag, MPI_COMM_WORLD, &status);
@@ -46,16 +50,18 @@ int main(int argc, char **argv)
 	  totaltime =end - start;
 
 	  printdata(data,N,Nlocal);
+
 	  std::cout << 1*sizeof(double) << "\t" << totaltime<< "\t" << 1*sizeof(double)/totaltime/1.0e6 <<std::endl;
         }
     } else {
-        MPI_Send(&data[0], N*Nlocal, MPI_DOUBLE, 0, tag , MPI_COMM_WORLD);
+        MPI_Send(&data[0], N*Nlocal, MPI_DOUBLE, 0, tag, MPI_COMM_WORLD);
     }
      MPI_Finalize();
    
     return 0;
 }
-void printdata(std::vector<double>&data,int N, int Nlocal){
+
+void printdata(std::vector<double>data,int N, int Nlocal){
   /*std::vector<double>data(N*Nlocal);
     std::fill(data.begin(),data.end(),0.0);
 

3f1cb9a2180e0906c41a5bc35cdceb3665edac88
Karen <kgomezo@unal.edu.co>
2022-01-19 00:38:10 -0500

    intento

--- 2022-01-14-MPIExercises/matrix.cpp
+++ 2022-01-14-MPIExercises/matrix.cpp
@@ -6,17 +6,14 @@
 #include <stdlib.h>
 
 void printdata(std::vector<double>&data,int N, int Nlocal);
-int np, pid;
-const int N = 6;
-int Nlocal=N/np;
+
 
 int main(int argc, char **argv)
 {
- // const int N = 8; //std::atoi(argv[1]);
- // int np , pid;
- // int Nlocal;
-//  Nlocal=N/np;
-    int tag =0;
+  const int N = std::atoi(argv[1]);
+  int np, pid;
+  int Nlocal=N/np;
+  int tag =0;
 
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &np);
@@ -25,31 +22,37 @@ int main(int argc, char **argv)
 
     std::vector<double>data(N*Nlocal);
     std::fill(data.begin(),data.end(),0.0);
+   
 
     for(int ilocal=0; ilocal<Nlocal;++ilocal){
 
         for(int jlocal = Nlocal*pid; jlocal < Nlocal*(pid+1);++jlocal) {
-            data[ilocal*(N+1)+jlocal]=1.0;
-            break;
+            data[ilocal*(N)+jlocal]=1.0;
+            //break;
 
         }
 
     }
+   
+    
+    double totaltime = 0;
 
-    if(pid =0){
+    if(pid == 0){
         printdata(data,N,Nlocal);
         for(int ipid=1; ipid < np;++ipid){
-            MPI_Recv(&data[0],N, MPI_DOUBLE, 1,ipid, MPI_COMM_WORLD, &status);
-            printdata(data,N,Nlocal);
+	  double start= MPI_Wtime();
+            MPI_Recv(&data[0], N*Nlocal, MPI_DOUBLE, ipid,tag, MPI_COMM_WORLD, &status);
+	  double end = MPI_Wtime();
+	  totaltime =end - start;
+
+	  printdata(data,N,Nlocal);
+	  std::cout << 1*sizeof(double) << "\t" << totaltime<< "\t" << 1*sizeof(double)/totaltime/1.0e6 <<std::endl;
         }
     } else {
-        MPI_Send(&data[0], N, MPI_DOUBLE, 0,0 , MPI_COMM_WORLD);
+        MPI_Send(&data[0], N*Nlocal, MPI_DOUBLE, 0, tag , MPI_COMM_WORLD);
     }
-
-
-
-   MPI_Finalize();
-
+     MPI_Finalize();
+   
     return 0;
 }
 void printdata(std::vector<double>&data,int N, int Nlocal){
@@ -68,7 +71,7 @@ void printdata(std::vector<double>&data,int N, int Nlocal){
 
     for (int i=0; i<Nlocal;++i){
         for(int j=0; j<N; j++){
-            std::cout << data[(i*N)+j]<<"\t";
+	  std::cout << data[(i*N)+j]<<"\t";
         }
         std::cout<<"\n";
     }
